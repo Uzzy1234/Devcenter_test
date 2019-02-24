7f31c15694710d8fc73d6bea01fcee76725017a4
Uzochukwu Onuegbu <uzochukwu.onuegbu@demzanalytics.com>
2019-02-24 12:20:21 +0100

    create model relationships for users and roles

--- app/Models/User.js
+++ app/Models/User.js
@@ -21,18 +21,12 @@ class User extends Model {
     })
   }
 
-  /**
-   * A relationship on tokens is required for auth to
-   * work. Since features like `refreshTokens` or
-   * `rememberToken` will be saved inside the
-   * tokens table.
-   *
-   * @method tokens
-   *
-   * @return {Object}
-   */
-  tokens () {
-    return this.hasMany('App/Models/Token')
+  static get hidden () {
+    return ['password','created_at','updated_at']
+  }
+
+  roles() {
+    return this.belongsToMany('App/Models/Role')
   }
 }
 
